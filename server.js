997bb3bafbbc6a48000e5173dec5508154f2067d
uazhar-code <uazhar.git@gmail.com>
2021-02-18 23:56:52 -0500

    Added functionality to generate tokens

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) =>{
 	res.send('success');
 })
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+app.post('/signin', (req, res) => { signin.signinAuthentication(req, res, db, bcrypt) });
 
 app.post('/register', (req,res) => { register.handleRegister(req, res, db, bcrypt) });
 
